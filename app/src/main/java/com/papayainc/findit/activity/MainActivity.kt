cb3f00e1096fa0d9f0ef2a791d69688905d023b4
Vladislav Erchik <vladislav.erchik@gmail.com>
2018-12-13 10:03:37 +0300

    Provide root sources

---
+++ app/src/main/java/com/papayainc/findit/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.papayainc.findit.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.papayainc.findit.R
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
